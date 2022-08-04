abd07bfdc599f60632be53b7c2ee3615e58a7e5b
vemulapallimtrain <110632725+vemulapallimtrain@users.noreply.github.com>
2022-08-04 16:40:55 -0400

    Update streamlit_app.py
    Added header for build your own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
