d71e8b0dd723fd23aef62935b21705eab04d16bb
vemulapallimtrain <110632725+vemulapallimtrain@users.noreply.github.com>
2022-08-08 13:43:44 -0400

    Update streamlit_app.py
    Correcting the spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 import requests
-fruityvice_respons=requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
