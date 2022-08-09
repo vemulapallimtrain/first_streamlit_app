bc0503a0f89424c9decca6258cb6605d6a0fb9b7
vemulapallimtrain <110632725+vemulapallimtrain@users.noreply.github.com>
2022-08-09 09:32:47 -0400

    Update streamlit_app.py
    Uncommenting imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('🥑 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
+import pandas
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
@@ -26,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
+import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) #Just writes the JSON data to the screen
 
