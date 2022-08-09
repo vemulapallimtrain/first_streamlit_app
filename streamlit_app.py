a389bf964213ab27e08b9b87bd5cc6b1707fe0c3
vemulapallimtrain <110632725+vemulapallimtrain@users.noreply.github.com>
2022-08-09 11:56:57 -0400

    Update streamlit_app.py
    Corrected syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #Create the repeatable code block (called a function)
-def get_fruityvise_data(this_fruit_choice);
+def get_fruityvise_data(this_fruit_choice):
   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
