9c4a91281b3106550113774378a31e535f279d12
vemulapallimtrain <110632725+vemulapallimtrain@users.noreply.github.com>
2022-08-09 08:58:38 -0400

    Update streamlit_app.py
    Corrected the syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.header("The fuit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 ##Allow the end user to add a fruit to the fruit list
-add_my_fruit = streamlit.text_input('What fruit would you like to add your fruit list?,'Kiwi')
+add_my_fruit = streamlit.text_input('What fruit would you like to add your fruit list?','Kiwi')
