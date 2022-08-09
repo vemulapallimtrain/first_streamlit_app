55e5bc560cb888bbde45ce4868a896a7cd144c5f
vemulapallimtrain <110632725+vemulapallimtrain@users.noreply.github.com>
2022-08-09 09:44:59 -0400

    Update streamlit_app.py
    Try-Except with a nested if-else

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+import requests
+from urllib.error import urlerror
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -10,7 +13,7 @@ streamlit.text('🥑 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
@@ -23,27 +26,25 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #Just writes the JSON data to the screen
-
-#Take the JSON version of the response and normalize it. 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#Output the normalized data to the screen as table
-streamlit.dataframe(fruityvice_normalized)
+try
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information")
+   esle: 
+      streamlit.write('The user entered ', fruit_choice)
+      fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      #streamlit.text(fruityvice_response.json()) #Just writes the JSON data to the screen
+      #Take the JSON version of the response and normalize it. 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      #Output the normalized data to the screen as table
+      streamlit.dataframe(fruityvice_normalized)
+  except urlerror as e:
+      streamlit.error()
 
 streamlit.stop()
 
 
 import snowflake.connector
-from urllib.error import urlerror
-
-
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
